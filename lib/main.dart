cb60675d6c0c71f970336e104772fbdc01268f75
Aditya Sahu <sahuaditya083@gmail.com>
2023-06-27 21:32:44 +0530

    quiz app code

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:dice/quiz.dart';
+import 'package:flutter/material.dart';
+
+import 'models/quiz.model.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const QuizApp();
+  }
+
+}
+
+
---
+++ lib/models/quiz.model.dart
@@ -0,0 +1,7 @@
+class QnA{
+  QnA({required this.question, required this.options, this.answer});
+
+  String question;
+  List<String> options;
+  String? answer;
+}
